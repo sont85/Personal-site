77e29e162a9b5cf0cfe3fa8f56f6f4c643b23f41
Son Truong <sont85@gmail.com>
2015-07-09 17:48:44 -0700

    expanded parralex

---
+++ script.js
@@ -0,0 +1,14 @@
+var app = angular.module("PersonalPage", []);
+app.directive("projectCard", function(){
+  return {
+    restrict: 'E',
+    templateUrl: "project.html"
+  };
+});
+
+app.controller("MainCtrl", function() {
+
+
+
+
+});
